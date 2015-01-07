0ae985c768c20cc771fb052fd48dfed2cfb17e78
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-01-07 02:25:24 -0500

    add changed json2csv file

--- PayForPerformance/Scripts/json2csv.js
+++ PayForPerformance/Scripts/json2csv.js
@@ -1,37 +1,59 @@
-// Module for parsing JSON into CSV
-var csvConverter = {
-  convertCSV: function (objArray) {
-    var array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-    var headers = Object.keys(array[0]);
-    return this.parseHeaders(headers, array);
-  },
-  parseHeaders: function(headers, array) {
-    var str = '';
-    headers.forEach(function(item) {
-      str += item + ',';
-    });
-    str += '\r\n';
-    return this.parseBody(array, str);
-  },
-  parseBody: function(array, str) {
-    var regex, value, line;
+(function() {
+  return csvConverter = {
+    convert: function (objArray) {
+      var array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+      //Assume that the headers of the document are equal to the keys in the JSON object. 
+      var headers = Object.keys(array[0]);
+      var stringWithHeaders = this.parseHeaders(headers, array);
+      var parsedString = this.parseBody(array, stringWithHeaders);
+      return this.open(parsedString);
+    },
 
-    array.forEach(function (item, index) {
-      line = '';
-      for (var index in item) {
-        if (line !== '') { line += ','; }
-        regex = /\,/
-        value = item[index]
-       
-        if (typeof value  === "string") {
-          if ( regex.test(value) ) {
-            value = '"' + value + '"'
+    parseHeaders: function(headers) {
+      //Push the headers into the CSV string. 
+      var str = '';
+      headers.forEach(function(item) {
+        str += item + ',';
+      });
+      str += '\r\n';
+      return str;
+    },
+
+    parseBody: function(array, str) {
+      var regex, value, line;
+
+      array.forEach(function (item, index) {
+        line = '';
+        for ( index in item ) {
+          if (  line !== '' ) line += ','; 
+          regex = /\,/;
+          value = item[index];
+
+          if (typeof value  === "string") {
+            // If the value contained in the JSON object is a string:
+            // Perform a regex test to check and see if the value has a comma already in place and escape the value. 
+            // e.g. "Smith, Jones" as a value should not be separated two different columns. 
+            value = regex.test(value) ? '"' + value + '"' : value;
           }
+          line += value;
         }
-        line += value;
+        str += line + '\r\n';
+      });
+      return str;
+    },
+
+    open: function(csvString) {
+      if (Object.hasOwnProperty.call(window, "ActiveXObject") && !window.ActiveXObject) 
+      {  // Determine if client is IE11
+        var blob = new Blob([csvString], {
+          type: "text/csv;charset=utf-8;"
+        });
+        return window.navigator.msSaveBlob(blob, "tcm-01.csv");
+      } 
+      else {
+        return window.open("data:text/csv;charset=utf-8," + escape(csvString));
       }
-      str += line + '\r\n';
-    });
-    return str;
+    csvString = null;
+    }
   }
-}
+}.call(this));
