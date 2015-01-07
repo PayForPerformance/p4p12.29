f0f56e0e2c496873c9ee940791fe75f941b3fc30
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-01-06 20:05:15 -0500

    angular app now renders html template dynamically

--- PayForPerformance/Scripts/app/app.js
+++ PayForPerformance/Scripts/app/app.js
@@ -1,4 +1,4 @@
-﻿var ComharNGApp = angular.module('ComharNGApp', ['comharControllers']);
+﻿var ComharNGApp = angular.module('ComharNGApp', ['ngSanitize','comharControllers']);
 
 
 ComharNGApp.directive('popoverClose', function() {
@@ -18,3 +18,45 @@ ComharNGApp.directive('jqueryCreate', function() {
     }
   };
 });
+
+ComharNGApp.directive('jqueryTwo', function() {
+ return {
+   link: function($scope, $element, $attr) {
+      $element.trigger('create');
+    }
+  };
+});
+
+
+ComharNGApp.directive('testKpi', function($compile) {
+  return {
+    link: function($scope, $element, $attr){
+    var template ='<div data-role="collapsible" data-collapsed-icon="arrow-r" data-collapsed="true" data-theme="b" data-content-theme="a">'
+                     +'<h3 ng-click="setSelectedId(kpi.programId)" id="container-{{ kpi.programId }}" class="kpiContainer"> {{ kpi.programName }}</h3>'
+                     +'<div data-role="collapsible" data-collapsed-icon="arrow-r" data-theme="b" data-collapsed="false" jquery-create>'
+                     +'<h3>Percent having a TCM contact within 2 days of inpatient admission</h3>'
+                     +'<div class="ui-grid-a ui-responsive">'
+                     +'<div class="ui-block-a" >'
+                     +'<div class-"ui-bar ui-bar-a" style="background-color:white;" jqeury-create>'
+                     +'<div id="chart-{{ kpi.programId }}" class="kpiChart"></div>'
+                     +'</div>'
+                     +'</div>'
+                     +'<div class="ui-block-b">'
+                     +'<div class="ui-bar ui-bar-c">'
+                     +'<div data-role="collapsible" ng-repeat ="item in data | filter: {  P4PInfoId : selectedId }" jquery-create>'
+                     +'<h3>{{ item.Heading }}</h3>'
+                     +'<p>{{ item.Text }}</p>'
+                     +'</div>'
+                     +'</div>'
+                     +'</div>'
+                     +'</div>'
+                     +'</div>'
+                   
+
+      var linkFn = $compile(template);
+      var content = linkFn($scope);
+      $element.append(content);
+    } 
+  }
+});
+
--- PayForPerformance/Scripts/app/controller.js
+++ PayForPerformance/Scripts/app/controller.js
@@ -2,9 +2,45 @@
 
 comharControllers.controller('comharCtrl', ['$scope', '$http',
   function ($scope, $http) {
+
   $http.get('Scripts/KPIInfo.js').success(function(data) {
     $scope.data = angular.fromJson(data);
-  });
+
+    //TODO - FIXME / DISCUSS
+    var programNames = [];
+    $scope.data.forEach(function(item) {
+      if (programNames.indexOf( item.KpiId ) === -1) {
+        return programNames.push(item.KpiId);
+      } 
+    });
+
+    var programIds = [];
+    var last = 0;
+     $scope.data.forEach(function(item) {
+      if (item.P4PInfoId > last) {
+        last = item.P4PInfoId;
+        return programIds.push(item.P4PInfoId);
+      } 
+    });
+    
+    $scope.programNames = programNames;
+    var namesandIds = [];
+    for (var _i = 0; _i < programNames.length; _i++) {
+      namesandIds.push({ 'programId' : programIds[_i], 'programName' : programNames[_i] }); 
+    }
+
+    //TODO FIXME
+    //TEMPORARY REMOVE FIRST ELEMENT
+    $scope.namesandIds = namesandIds.splice(1, namesandIds.length);
+
+    });
+    programIds = null;
+    programNames = null;
+
+    $scope.setSelectedId = function(id) {
+      $scope.selectedId = id;
+
+    };
 }]);
 
 
@@ -27,9 +63,8 @@ comharControllers.controller('dateCtrl', ['$rootScope', '$scope', '$http',
 
       $scope.setSelected = function(index) {
         $rootScope.selected = $scope.years[index];
-      }
+      };
      
 
   });
 }]);
-
