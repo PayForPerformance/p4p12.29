7d4c56112a960ef298a209cbabe426075f13e846
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-01-07 19:20:33 -0500

    remove code and clean up controller

--- PayForPerformance/Scripts/app/controller.js
+++ PayForPerformance/Scripts/app/controller.js
@@ -1,46 +1,17 @@
 ﻿var comharControllers = angular.module('comharControllers', []);
 
-comharControllers.controller('comharCtrl', ['$scope', '$http',
-  function ($scope, $http) {
+comharControllers.controller('comharCtrl', ['$scope', '$http', 'comharFunctions',
+  function ($scope, $http, comharFunctions) {
 
   $http.get('Scripts/KPIInfo.js').success(function(data) {
     $scope.data = angular.fromJson(data);
 
-    //TODO - FIXME / DISCUSS
-    var programNames = [];
-    $scope.data.forEach(function(item) {
-      if (programNames.indexOf( item.KpiId ) === -1) {
-        return programNames.push(item.KpiId);
-      } 
-    });
-
-    var programIds = [];
-    var last = 0;
-     $scope.data.forEach(function(item) {
-      if (item.P4PInfoId > last) {
-        last = item.P4PInfoId;
-        return programIds.push(item.P4PInfoId);
-      } 
-    });
-    
-    $scope.programNames = programNames;
-    var namesandIds = [];
-    for (var _i = 0; _i < programNames.length; _i++) {
-      namesandIds.push({ 'programId' : programIds[_i], 'programName' : programNames[_i] }); 
-    }
-
-    //TODO FIXME
-    //TEMPORARY REMOVE FIRST ELEMENT
-    $scope.namesandIds = namesandIds.splice(1, namesandIds.length);
-
-    });
-    programIds = null;
-    programNames = null;
+    $scope.namesandIds = comharFunctions.getNamesAndIds(data);
 
     $scope.setSelectedId = function(id) {
       $scope.selectedId = id;
-
     };
+  });
 }]);
 
 
