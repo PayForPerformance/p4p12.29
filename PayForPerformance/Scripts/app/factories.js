1e2b1b33c4900bbab3150c16603bdecf7d665e64
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-01-29 23:12:52 -0500

    much cleaner getnamesandIds f(x) thanks to _

--- PayForPerformance/Scripts/app/factories.js
+++ PayForPerformance/Scripts/app/factories.js
@@ -3,27 +3,17 @@
 comharFunctions.factory('comharFunctions', function() {
   return {
     getNamesAndIds : function(data) {
-      var programNames = [];
-      data.forEach(function(item) {
-        if (programNames.indexOf( item.KpiId ) === -1) {
-          return programNames.push(item.KpiId);
-        } 
-      });
 
-      var programIds = [];
-      var last = 0;
-       data.forEach(function(item) {
-        if (item.P4PInfoId > last) {
-          last = item.P4PInfoId;
-          return programIds.push(item.P4PInfoId);
-        } 
-      });
+      var programNames = _.uniq(data.map(function(item) { return item.KpiId; }));
+
+      var programIds = _.uniq(data.map(function(item) { return item.P4PInfoId; }));
+
       var namesandIds = [];
       for (var _i = 0; _i < programNames.length; _i++) {
         namesandIds.push({ 'programId' : programIds[_i], 'programName' : programNames[_i] }); 
       }
 
-      //TODO FIXME
+      //TODO
       //TEMPORARY REMOVE FIRST ELEMENT
       namesandIds = namesandIds.splice(1, namesandIds.length);
       return namesandIds;
